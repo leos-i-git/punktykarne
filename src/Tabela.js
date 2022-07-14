37eded80e59cc450aca7b7f384dd995a40a02f57
leos-i-git <71249613+leos-i-git@users.noreply.github.com>
2022-07-14 16:00:59 +0200

    Update Tabela.js

--- src/Tabela.js
+++ src/Tabela.js
@@ -16,7 +16,6 @@ class Tabela extends Component {
 
 			var dateTime = sessionStorage.getItem("Data");
 			console.log(dateTime);
-			document.getElementById("dacisko").innerHTML=""+dateTime+"";
 			var kierowca = sessionStorage.getItem("Kierowca");
 			var flaga = sessionStorage.getItem("Flaga");
 			var seria = sessionStorage.getItem("Seria");
@@ -46,51 +45,67 @@ class Tabela extends Component {
 	
 			if (flaga === "at"){
 				obrazek.setAttribute("src", "/images/flags/png/at.png");
+				obrazek.setAttribute("alt","Austria");
 			}
 			else if (flaga === "au"){
 				obrazek.setAttribute("src", "/images/flags/png/au.png");
+				obrazek.setAttribute("alt","Australia");
 			}
 			else if (flaga === "be"){
 				obrazek.setAttribute("src", "/images/flags/png/be.png");
+				obrazek.setAttribute("alt","Belgia");
 			}
 			else if (flaga === "bh"){
 				obrazek.setAttribute("src", "/images/flags/png/bh.png");
+				obrazek.setAttribute("alt","Bahrain");
 			}
 			else if (flaga === "ca"){
 				obrazek.setAttribute("src", "/images/flags/png/ca.png");
+				obrazek.setAttribute("alt","Kanada");
 			}
 			else if (flaga === "cz"){
 				obrazek.setAttribute("src", "/images/flags/png/cz.png");
+				obrazek.setAttribute("alt","Czechy");
 			}
 			else if (flaga === "de"){
 				obrazek.setAttribute("src", "/images/flags/png/de.png");
+				obrazek.setAttribute("alt","Niemcy");
 			}
 			else if (flaga === "es"){
 				obrazek.setAttribute("src", "/images/flags/png/es.png");
+				obrazek.setAttribute("alt","Hiszpania");
 			}
 			else if (flaga === "hu"){
 				obrazek.setAttribute("src", "/images/flags/png/hu.png");
+				obrazek.setAttribute("alt","Węgry");
 			}
 			else if (flaga === "it"){
 				obrazek.setAttribute("src", "/images/flags/png/it.png");
+				obrazek.setAttribute("alt","Włochy");
 			}
 			else if (flaga === "kr"){
 				obrazek.setAttribute("src", "/images/flags/png/kr.png");
+				obrazek.setAttribute("alt","Korea");
 			}
 			else if (flaga === "mc"){
 				obrazek.setAttribute("src", "/images/flags/png/mc.png");
+				obrazek.setAttribute("alt","Monako");
 			}
 			else if (flaga === "nl"){
 				obrazek.setAttribute("src", "/images/flags/png/nl.png");
+				obrazek.setAttribute("alt","Holandia");
 			}
 			else if (flaga === "sa"){
 				obrazek.setAttribute("src", "/images/flags/png/sa.png");
+				obrazek.setAttribute("alt","Arabia Saudyjska");
 			}
 			else if (flaga === "th"){
 				obrazek.setAttribute("src", "/images/flags/png/th.png");
+				obrazek.setAttribute("alt","Tajlandia");
 			}
 			else if (flaga === "uk"){
 				obrazek.setAttribute("src", "/images/flags/png/uk.png");
+				obrazek.setAttribute("alt","Wielka Brytania");
 			}
 			
 			
@@ -133,7 +148,7 @@ class Tabela extends Component {
 			pokazkoda();
 		}
 		function pokazkoda(){
-			var d = document.getElementById("jazda");
+			var d = document.getElementById("tabeleczka");
 			console.log(d.innerHTML);
 		}
 
@@ -142,69 +157,10 @@ class Tabela extends Component {
 			
             <div>
 				<div id="jazda">
-                <table id='tabelka'>
-	<thead>
-		<tr>
-			<th><b>ŁĄCZNIE</b></th>
-			<th><b>KTO</b></th>
-			<th><b>Seria F1</b></th>
-			<th><b>Seria F3</b></th>
-			<th><b>Seria ATC</b></th>
-			<th><b>Seria GT</b></th>
-			<th><b>Eventy</b></th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td id="Tomaszpkt">11</td>
-			<td >Tomasz</td>
-			<td id="Tomaszf1"></td> 
-			<td id="Tomaszf3"></td>
-			<td id="Tomaszatc"><img src="/images/flags/png/de.png" title="R0 Nurburgring" /> 3<img src="/images/flags/png/kr.png" title="R6 Korea"/> 2 </td>
-			<td id="Tomaszgt"></td>
-			<td id="Tomaszev"><img src="/images/flags/png/it.png" title="McLaren Challenge"/> 6 </td>
-		</tr>
-		<tr>
-			<td id="WojciechNowakpkt">15</td>
-			<td >Wojciech Nowak</td>
-			<td id="WojciechNowakf1"></td>
-			<td id="WojciechNowakf3"></td>
-			<td id="WojciechNowakatc"><img src="/images/flags/png/de.png" title="R0 Nurburgring"/> 5</td>
-			<td id="WojciechNowakgt"><img src="/images/flags/png/it.png" title="R0 Imola"/> 2 <img src="/images/flags/png/uk.png" title="R3 Outlon Park"/> 2 </td>
-			<td id="WojciechNowakev"></td>
-		</tr>
-		<tr>
-			<td id="Mariuszpkt">9</td>
-			<td>Mariusz</td>
-			<td id="Mariuszf1"></td>
-			<td id="Mariuszf3"><img src="/images/flags/png/bh.png" title="R4 Bahrain"/> 1 <img src="/images/flags/png/sa.png" title="R5 Jeddah"/> 1 </td>
-			<td id="Mariuszatc"><img src="/images/flags/png/cz.png" title="R1 Most"/> 4 <img src="/images/flags/png/es.png" title="R2 Jarama"/> 1<br /><img src="/images/flags/png/hu.png" title="R3 Hungaroring"/> 1 </td>
-			<td id="Mariuszgt"></td>
-			<td id="Mariuszev"><img src="/images/flags/png/it.png" title="McLaren Challenge"/> 1</td>
-		</tr>
-		<tr>
-			<td id="Adampkt">9</td>
-			<td>Adam</td>
-			<td id="Adamf1"></td>
-			<td id="Adamf3"><img src="/images/flags/png/bh.png" title="R4 Bahrain"/> 2</td>
-			<td id="Adamatc"><img src="/images/flags/png/hu.png" title="R3 Hungaroring"/> 2<img src="/images/flags/png/th.png" title="R5 Chang"/> 5</td>
-			<td id="Adamgt"></td>
-			<td id="Adamev"></td>
-		</tr>
-	</tbody>
-  <tfoot>
-		<tr>
-			<th></th>
-			<th>Aktualizacja:</th>
-			<th id ="dacisko">0000-00-00 00:00:00</th>
-			<th></th>
-			<th></th>
-			<th></th>
-			<th></th>
-		</tr>
-	</tfoot>
-</table>
-</div>
+		
+
+
+				</div>
 <button onClick={submit}>Załaduj Dane do Tabeli</button>
 
             </div>
